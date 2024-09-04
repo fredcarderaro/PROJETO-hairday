865357ca1358adbc447aa00a013e6ab0837cf772
fredcarderaro <fredcarderaro@gmail.com>
2024-09-04 20:55:03 -0300

    Starting the API Iteration

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict"  // Modo estrito
+
+// Configuração do dayjs
+import "./libs/dayjs.js";
+
+// CSS
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
+
+// TESTANDO O DAYJS
+// import dayjs from "dayjs";
+// console.log(dayjs().format("HH:mm"));
